8e10c977c57577e09070189e9e08109873a35ed0
Ума Мирзоева <vselennaya314@gmail.com>
2025-03-25 04:10:57 +0100

    chore(project): initial SwiftUI project setup

---
+++ WikipediaPlacesLauncher/WikipediaPlacesLauncherApp.swift
@@ -0,0 +1,17 @@
+//
+//  WikipediaPlacesLauncherApp.swift
+//  WikipediaPlacesLauncher
+//
+//  Created by Ума Мирзоева on 25/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct WikipediaPlacesLauncherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
