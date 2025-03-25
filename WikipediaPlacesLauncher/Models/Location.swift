9232874805d2b3fbb132349a9ac9671fa68ef29e
Ума Мирзоева <vselennaya314@gmail.com>
2025-03-25 05:48:43 +0100

    refactor(model): simplify Location model using default Codable behavior

--- WikipediaPlacesLauncher/Models/Location.swift
+++ WikipediaPlacesLauncher/Models/Location.swift
@@ -8,7 +8,7 @@
 import Foundation
 
 struct Location: Codable, Identifiable {
-    let id: UUID
+    let id = UUID()
     let name: String?
     let latitude: Double
     let longitude: Double
@@ -18,21 +18,4 @@ struct Location: Codable, Identifiable {
         case latitude = "lat"
         case longitude = "long"
     }
-
-    init(name: String?, latitude: Double, longitude: Double) {
-        self.id = UUID()
-        self.name = name
-        self.latitude = latitude
-        self.longitude = longitude
-    }
-
-    init(from decoder: Decoder) throws {
-        let container = try decoder.container(keyedBy: CodingKeys.self)
-        self.name = try container.decodeIfPresent(String.self, forKey: .name)
-        self.latitude = try container.decode(Double.self, forKey: .latitude)
-        self.longitude = try container.decode(Double.self, forKey: .longitude)
-        self.id = UUID()
-    }
 }
-
-
